4598a121454ee8bb73b1ed99f79d7bd6d4f53ab8
Greșanu Emanuel Vasile <emanuel2558@gmail.com>
2019-10-19 10:46:13 +0300

    add build configurations in pomn file, added project dependencies in the pomn file and create a first demo to see if every thing is ok.

--- src/main/java/runnable/MainTEST.java
+++ src/main/java/runnable/MainTEST.java
@@ -1,9 +1,11 @@
 package runnable;
 
+import util.HibernateHelper;
+
 public class MainTEST {
     public static void main(String[] args) {
         System.out.println("Functioneaza");
-
+        HibernateHelper inst = HibernateHelper.getInstance();
         
     }
 }
