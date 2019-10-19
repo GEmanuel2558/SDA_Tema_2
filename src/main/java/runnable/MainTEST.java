aad11cd41e13b025ff5102eee64a54f5b4f148a1
Greșanu Emanuel Vasile <emanuel2558@gmail.com>
2019-10-19 14:43:00 +0300

    replace hardcoded password with sha256 algorithm to see if it works

--- src/main/java/runnable/MainTEST.java
+++ src/main/java/runnable/MainTEST.java
@@ -1,13 +1,16 @@
 package runnable;
 
+import com.google.common.hash.Hashing;
 import dao.repositorys.PermissionCheckerDao;
 import util.HibernateHelper;
+
+import java.nio.charset.StandardCharsets;
 
 public class MainTEST {
     public static void main(String[] args) {
         System.out.println("Functioneaza");
         //Demo only. The below line will pe deleted.
         PermissionCheckerDao a =new PermissionCheckerDao();
-        a.findUserByNameAndPassword("Emanuel", "a26e622917d97103958043fc757d1364d684e7d2eb262b1b4e64ec666b042f58");
+        a.findUserByNameAndPassword("Emanuel", Hashing.sha256().hashString("Emanuel", StandardCharsets.UTF_8).toString());
     }
 }
